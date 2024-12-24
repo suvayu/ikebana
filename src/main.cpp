b9f2ec817393b383ad60c6d17dcb8282b7eb87d2
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-12-24 11:34:22 +0100

    main.cpp: attempt to fix segfault

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <fmt/core.h>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
@@ -7,43 +7,41 @@
 #include <GLFW/glfw3.h>
 
 #include <duckdb.h>
-#include <duckdb/main/connection.hpp>
 
 void materialise_results(duckdb_result &res) {
-  duckdb_data_chunk result = duckdb_fetch_chunk(res);
-  if (!result) {
+  duckdb_data_chunk chunk = duckdb_fetch_chunk(res);
+  if (!chunk) {
     // result is exhausted
     return;
   }
   // get the number of rows from the data chunk
-  idx_t row_count = duckdb_data_chunk_get_size(result);
+  idx_t row_count = duckdb_data_chunk_get_size(chunk);
   // get the first column
-  duckdb_vector col1 = duckdb_data_chunk_get_vector(result, 0);
+  duckdb_vector col1 = duckdb_data_chunk_get_vector(chunk, 0);
   int32_t *col1_data = (int32_t *)duckdb_vector_get_data(col1);
   uint64_t *col1_validity = duckdb_vector_get_validity(col1);
 
   // get the second column
-  duckdb_vector col2 = duckdb_data_chunk_get_vector(result, 1);
-  char **col2_data = (char **)duckdb_vector_get_data(col2);
+  duckdb_vector col2 = duckdb_data_chunk_get_vector(chunk, 1);
+  duckdb_string_t *col2_data = (duckdb_string_t *)duckdb_vector_get_data(col2);
   uint64_t *col2_validity = duckdb_vector_get_validity(col2);
 
   // iterate over the rows
   for (idx_t row = 0; row < row_count; row++) {
     if (duckdb_validity_row_is_valid(col1_validity, row)) {
-      printf("%d", col1_data[row]);
+      fmt::print("{:d}, {}", col1_data[row]);
     } else {
-      printf("NULL");
+      fmt::print("NULL");
     }
-    printf(",");
     if (duckdb_validity_row_is_valid(col2_validity, row)) {
-      printf("%s", col2_data[row]);
+      fmt::print("{:d}", col2_data[row].value.pointer.length);
     } else {
-      printf("NULL");
+      fmt::print("NULL");
     }
-    printf("\n");
-    ImGui::Text("%d\t%s", col1_data[row], col2_data[row]);
+    fmt::print("\n");
   }
-  duckdb_destroy_data_chunk(&result);
+  // ImGui::Text("%d\t%s", col1_data[row], col2_data[row]);
+  duckdb_destroy_data_chunk(&chunk);
 }
 
 int main() {
@@ -102,7 +100,9 @@ int main() {
       // handle error
     }
 
-    materialise_results(result);
+    ImGui::Text("%d\t%s", 1, "foo");
+    ImGui::Text("%d\t%s", 2, "bar");
+    ImGui::Text("%d\t%s", 314, "baz");
     ImGui::End();
 
     // Rendering
