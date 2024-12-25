3d36bda40b8b2772869ae2d8287d86ed12588a2c
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-12-25 04:14:21 +0100

    src/status.h: add trailing newline to log message

--- src/status.h
+++ src/status.h
@@ -5,11 +5,10 @@
 #include <duckdb.h>
 #include <fmt/core.h>
 
-// FIXME: add newline to message
 #define DUCKDB_STATE_OK(status, msg_fmt, ...)                                  \
   do {                                                                         \
     if (status == DuckDBError)                                                 \
-      fmt::print(msg_fmt __VA_OPT__(, ) __VA_ARGS__);                          \
+      fmt::print(msg_fmt "\n" __VA_OPT__(, ) __VA_ARGS__);                     \
   } while (0)
 
 #endif
