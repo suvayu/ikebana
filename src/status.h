cd70160ebf0f57266416d215c9358615eb758be5
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-12-30 17:47:28 +0100

    status.h: cleaner trailing newline in messages

--- src/status.h
+++ src/status.h
@@ -8,7 +8,7 @@
 #define DUCKDB_STATE_OK(status, msg_fmt, ...)                                  \
   do {                                                                         \
     if (status == DuckDBError)                                                 \
-      fmt::print(msg_fmt "\n" __VA_OPT__(, ) __VA_ARGS__);                     \
+      fmt::println(msg_fmt __VA_OPT__(, ) __VA_ARGS__);                        \
   } while (0)
 
 #endif
